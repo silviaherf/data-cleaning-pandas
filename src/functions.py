c29aece4475b6c69808f5d0ac8555845d5d2cc7e
Silvia Hernandez <silviaherf@gmail.com>
2020-08-30 12:34:22 +0200

    Intento de diagrama de quesito

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,8 @@
 #Funciones utilizadas en el archivo de código jupyter notebook
 import re
+import numpy as np
 def reference(x):
     return re.split("shark",x)
+
+def percentage(x,y):
+    return x/len(y)
