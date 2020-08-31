ca8f18d2e969976894228672b91a4a95bb1774e6
Silvia Hernandez <silviaherf@gmail.com>
2020-08-31 15:50:03 +0200

    fin jupyter y functions no utilizado

--- src/functions.py
+++ src/functions.py
@@ -1,39 +1,37 @@
-#Funciones utilizadas en el archivo de código jupyter notebook
+##Funciones utilizadas en el archivo de código jupyter notebook
 
+##Esta función finalmente no se utiliza
 
-"""
-Finalmente, no utilizo esta función
 def depure_activity(x):
-    if x.str.replace(r"(.*)urfing(.*)",'Surfing')==True:
-        x=x.str.replace(r"(.*)urfing(.*)",'Surfing')
-        return x
+    if type(x)==str:
+        if x.replace(r"(.*)urfing(.*)",'Surfing')==True:
+            x=x.replace(r"(.*)urfing(.*)",'Surfing')
+            return x
+        if x.replace(r"(.*)wimming(.*)",'Swimming')==True:
+            x=x.replace(r"(.*)wimming(.*)",'Swimming')
+            return x
+        if x.replace(r"(.*)ishing(.*)",'Fishing')==True:
+            x=x.replace(r"(.*)ishing(.*)",'Fishing')
+            return x
+        if x.replace(r"(.*)pearfishing(.*)",'Spearfishing')==True:
+            x=x.replace(r"(.*)pearfishing(.*)",'Spearfishing')
+            return x
+        if x.replace(r"(.*)athing(.*)",'Bathing')==True:
+            x=x.replace(r"(.*)bathing(.*)",'Bathing')
+            return x
+        if x.replace(r"(.*)Wading(.*)",'Wading')==True:
+            x=x.replace(r"(.*)Wading(.*)",'Wading')
+            return x
+        if x.replace(r"(.*)Diving(.*)",'diving')==True:
+            x=x.replace(r"(.*)Diving(.*)",'diving')
+            return x
+        if x.replace(r"(.*)tanding(.*)",'Standing')==True:
+            x=x.replace(r"(.*)tanding(.*)",'Standing')
+            return x
+        if x.replace(r"(.*)norkeling(.*)",'Snorkeling')==True:
+            x=x.replace(r"(.*)norkeling(.*)",'Snorkeling')
+            return x
+        if x.replace(r"(.*)Scuba diving(.*)",'Scuba diving')==True:
+            x=x.replace(r"(.*)Scuba diving(.*)",'Scuba diving')
+            return x
 
-    if x.str.replace(r"(.*)wimming(.*)",'Swimming')==True:
-        x=x.str.replace(r"(.*)wimming(.*)",'Swimming')
-        return x
-    if x.str.replace(r"(.*)ishing(.*)",'Fishing')==True:
-        x=x.str.replace(r"(.*)ishing(.*)",'Fishing')
-        return x
-    if x.str.replace(r"(.*)pearfishing(.*)",'Spearfishing')==True:
-        x=x.str.replace(r"(.*)pearfishing(.*)",'Spearfishing')
-        return x
-    if x.str.replace(r"(.*)athing(.*)",'Bathing')==True:
-        x=x.str.replace(r"(.*)bathing(.*)",'Bathing')
-        return x
-    if x.str.replace(r"(.*)Wading(.*)",'Wading')==True:
-        x=x.str.replace(r"(.*)Wading(.*)",'Wading')
-        return x
-    if x.str.replace(r"(.*)Diving(.*)",'diving')==True:
-        x=x.str.replace(r"(.*)Diving(.*)",'diving')
-        return x
-    if x.str.replace(r"(.*)tanding(.*)",'Standing')==True:
-        x=x.str.replace(r"(.*)tanding(.*)",'Standing')
-        return x
-    if x.str.replace(r"(.*)norkeling(.*)",'Snorkeling')==True:
-        x=x.str.replace(r"(.*)norkeling(.*)",'Snorkeling')
-        return x
-    if x.str.replace(r"(.*)Scuba diving(.*)",'Scuba diving')==True:
-        x=x.str.replace(r"(.*)Scuba diving(.*)",'Scuba diving')
-        return x
-
-"""
