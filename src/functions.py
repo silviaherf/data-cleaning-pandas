6a6f9be84267e3fbdffcf3b2e1c5410af3a43c7f
Silvia Hernandez <silviaherf@gmail.com>
2020-08-31 15:08:22 +0200

    archivo functions no utilizado

--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,39 @@
 #Funciones utilizadas en el archivo de código jupyter notebook
-import re
-import numpy as np
 
 
-def depure(x,current,new):
-    if x==current:
-        x=new
-    return x
+"""
+Finalmente, no utilizo esta función
+def depure_activity(x):
+    if x.str.replace(r"(.*)urfing(.*)",'Surfing')==True:
+        x=x.str.replace(r"(.*)urfing(.*)",'Surfing')
+        return x
+
+    if x.str.replace(r"(.*)wimming(.*)",'Swimming')==True:
+        x=x.str.replace(r"(.*)wimming(.*)",'Swimming')
+        return x
+    if x.str.replace(r"(.*)ishing(.*)",'Fishing')==True:
+        x=x.str.replace(r"(.*)ishing(.*)",'Fishing')
+        return x
+    if x.str.replace(r"(.*)pearfishing(.*)",'Spearfishing')==True:
+        x=x.str.replace(r"(.*)pearfishing(.*)",'Spearfishing')
+        return x
+    if x.str.replace(r"(.*)athing(.*)",'Bathing')==True:
+        x=x.str.replace(r"(.*)bathing(.*)",'Bathing')
+        return x
+    if x.str.replace(r"(.*)Wading(.*)",'Wading')==True:
+        x=x.str.replace(r"(.*)Wading(.*)",'Wading')
+        return x
+    if x.str.replace(r"(.*)Diving(.*)",'diving')==True:
+        x=x.str.replace(r"(.*)Diving(.*)",'diving')
+        return x
+    if x.str.replace(r"(.*)tanding(.*)",'Standing')==True:
+        x=x.str.replace(r"(.*)tanding(.*)",'Standing')
+        return x
+    if x.str.replace(r"(.*)norkeling(.*)",'Snorkeling')==True:
+        x=x.str.replace(r"(.*)norkeling(.*)",'Snorkeling')
+        return x
+    if x.str.replace(r"(.*)Scuba diving(.*)",'Scuba diving')==True:
+        x=x.str.replace(r"(.*)Scuba diving(.*)",'Scuba diving')
+        return x
+
+"""
